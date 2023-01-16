0206660751cc17ee7bb579b836d9c3791b4cdfb5
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-01-16 15:10:18 -0300

    structuring project and Connect to  mongoDB

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+// import router from './router';
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017')
+  .then(()=>{
+    const app = express();
+    const port = 3001;
+
+
+    app.use((req,res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+
+    });
+
+    app.use(express.json());
+    // app.use(router);
+
+    app.get('/', (req,res) => res.send('FALA  MEUS MAAAAANNOS E TRUTENS'));
+    app.listen(port, ()=> console.log(`server is running on: https://localhost:${port}`));
+  })
+  .catch((error) => console.log('Error to connect on mongoDB: \n ', error));
+
