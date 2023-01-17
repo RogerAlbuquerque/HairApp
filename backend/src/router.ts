eeb2231fbc12d4dd05ae52f3bf70c7a667c831f7
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-01-16 23:20:44 -0300

    Delete client script OK

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 
 import { createClient } from './app/useCases/clients/createClient';
+import { deleteClient } from './app/useCases/clients/deleteClient';
 import { showClient } from './app/useCases/clients/showClients';
 
 
@@ -11,3 +12,5 @@ export const router = Router();
 router.get('/client', showClient);
 
 router.post('/client', createClient);
+
+router.delete('/client/:id', deleteClient);
