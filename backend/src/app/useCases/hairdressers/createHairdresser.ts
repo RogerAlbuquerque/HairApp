0a4d2993322c5fd6b563ab83fab953a19608a5a7
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-01-17 12:49:33 -0300

    Implemented conditional with emaisl e usename when create hairdresser client

--- backend/src/app/useCases/hairdressers/createHairdresser.ts
+++ backend/src/app/useCases/hairdressers/createHairdresser.ts
@@ -4,18 +4,25 @@ import { Hairdresser } from '../../models/Hairdresser';
 
 export async function createHairdresser(req: Request, res:Response){
   const hairdresserData = req.body;
-  try{
-    bcrypt.genSalt(10, (error, salt) => {
-      bcrypt.hash(hairdresserData.hairdPassword, salt, async (error, hash) => {
-        hairdresserData.hairdPassword = hash;
-        const hairdresser = await Hairdresser.create(hairdresserData);
-        res.status(201).json(hairdresser);
-      });
+  const hairdExist = await Hairdresser.findOne({ $or: [ { hairdName: hairdresserData.hairdName }, { email:hairdresserData.email } ] });
 
-    });
+
+  try{
+    if(hairdExist){
+      res.status(500).json({error:'hairdName or email is already in use'});
+    }else{
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(hairdresserData.hairdPassword, salt, async (error, hash) => {
+          hairdresserData.hairdPassword = hash;
+          // const hairdresser = await Hairdresser.create(hairdresserData);
+          res.status(201).json(hairdresserData);
+        });
+      });
+    }
 
   }catch(error){
     console.log(error);
-    res.status(500).json({error:'Internal Server Error!'});
+    res.status(201).json({error:'Internal Server Error!'});
+
   }
 }
