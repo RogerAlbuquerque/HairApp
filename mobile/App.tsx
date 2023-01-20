d1f12dd95bac3407f345f7d161fa2998cf99a57a
PedroSetti <sr.setti@outlook.com>
2023-01-20 04:52:29 -0300

    create screen longin and your styled!

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
-import  React from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import Login from './src/components/Login/login';
 
 export default function App() {
   return (
+
     <View>
       <Login/>
       <StatusBar style="auto" />
---
+++ mobile/src/components/Login/style.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components/native';
+
+
+const TextInput = styled.TextInput`font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
