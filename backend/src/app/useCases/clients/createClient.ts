95011ad1d2185a4a5a86bdf94fc1de2f74361cd4
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-01-19 22:57:28 -0300

    Add conditional on creation of client and hairdresser if both have a  username or email equal the req

--- backend/src/app/useCases/clients/createClient.ts
+++ backend/src/app/useCases/clients/createClient.ts
@@ -1,25 +1,34 @@
 import { Request, Response } from 'express';
 import {Client} from '../../models/Client';
+import { Hairdresser } from '../../models/Hairdresser';
 import bcrypt from 'bcryptjs';
 
 export async function createClient(req: Request, res:Response){
   const clientData = req.body;
+
+  const hairdExist = await Hairdresser.findOne({$or:[{ clientName: clientData.clientName},{email:clientData.email}]});
+  const clientExist = await Client.findOne({$or:[{ clientName: clientData.clientName},{email:clientData.email}]});
+
+
   try{
-    bcrypt.genSalt(10, (error, salt) => {
-      bcrypt.hash(clientData.clientPassword, salt, async (error, hash) => {
-        clientData.clientPassword = hash;
-        clientData.clientName = clientData.clientName.replace(/\s+/g, '-');
-        try{
+    if(hairdExist || clientExist){
+      res.status(500).json({error:'user or email is already in use'});
+    }else{
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(clientData.hairdPassword, salt, async (error, hash) => {
+
+          clientData.clientPassword = hash;
+          clientData.clientName = clientData.clientName.replace(/\s+/g, '-');
+
           const client = await Client.create(clientData);
           res.status(201).json(client);
-        }catch(error){
-          console.log(error);
-          res.status(500).json({error:'Internal Server Error!'});
-        }
+        });
       });
-    });
+    }
+
   }catch(error){
     console.log(error);
-    res.status(500).json({error:'Internal Server Error!'});
+    res.status(201).json({error:'Internal Server Error!'});
+
   }
 }
