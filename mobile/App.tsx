9c69fb1b7a9fdbdc72211ae28ff2619d03e33570
PedroSetti <sr.setti@outlook.com>
2023-01-21 02:39:32 -0300

    Progress screen login for sab.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import Login from './src/components/Login';
 import { useFonts } from 'expo-font';
 
 export default function App() {
@@ -19,7 +18,6 @@ export default function App() {
   return (
 
     <View>
-      <Login/>
       <StatusBar style="auto" />
     </View>
   );
