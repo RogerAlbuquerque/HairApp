90b6e50ff445666a64753849b0e61fd3e8da2037
PedroSetti <sr.setti@outlook.com>
2023-01-21 02:18:20 -0300

    new commit from app

---
+++ mobile/src/components/Login/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { TextInput, View} from 'react-native';
+import { Text } from '../Text';
+import { Container, ContLogin, Footer} from './style';
+
+export default function Login(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Container>
+
+    </Container>
+  );
+}
