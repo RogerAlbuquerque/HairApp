3622a39b2a1cd0d956bc81223a9481a6e7f60017
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-01-22 17:05:15 -0300

    'updateClientInfo' add a test to see if email or password does exist

--- backend/src/app/useCases/clients/updateClientInfo.ts
+++ backend/src/app/useCases/clients/updateClientInfo.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { Client } from '../../models/Client';
+import { Hairdresser } from '../../models/Hairdresser';
 import bcrypt from 'bcryptjs';
 
 interface userInfo{
@@ -14,26 +15,34 @@ export async function updateClientInfo(req: Request, res:Response){
   const infos:userInfo = req.body;
   infos.clientName = infos.clientName?.replace(/\s+/g, '-');
 
+
+  const hairdExist = await Hairdresser.findOne({$or:[{ clientName: infos.clientName},{email:infos.email}]});
+  const clientExist = await Client.findOne({$or:[{ clientName: infos.clientName},{email:infos.email}]});
+
   try{
-    if(infos.clientPassword){
-      bcrypt.genSalt(10, (error, salt) => {
-        bcrypt.hash(infos.clientPassword!, salt, async (error, hash) => {
+    if(hairdExist || clientExist){
+      res.status(500).json('Username or email is alredy in use');
+    }else{
+      if(infos.clientPassword){
+        bcrypt.genSalt(10, (error, salt) => {
+          bcrypt.hash(infos.clientPassword!, salt, async (error, hash) => {
 
-          infos.clientPassword= hash;
+            infos.clientPassword= hash;
 
-          await Client.findOneAndUpdate({clientName: username}, infos).then(()=>{
-            res.status(200).json(infos);
-          }).catch((err)=>{
-            console.log(err);
-            res.status(500).json({error:'Internal Server Error!'});
+            await Client.findOneAndUpdate({clientName: username}, infos).then(()=>{
+              res.status(200).json(infos);
+            }).catch((err)=>{
+              console.log(err);
+              res.status(500).json({error:'Internal Server Error!'});
+            });
           });
         });
-      });
-    }else{
-      await Client.findOneAndUpdate({clientName: username}, infos).then(()=>{
-        res.status(200).json(infos);
-      }).catch(err => res.status(500).json({error:'Internal Server Error!'}));
+      }else{
+        await Client.findOneAndUpdate({clientName: username}, infos).then(()=>{
+          res.status(200).json(infos);
+        }).catch(err => res.status(500).json({error:'Internal Server Error!'}));
 
+      }
     }
 
   }catch(error){
