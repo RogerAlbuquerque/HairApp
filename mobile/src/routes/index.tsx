9e5faba42c5830823669d738bbc652260e8c9b45
PedroSetti <sr.setti@outlook.com>
2023-01-23 16:26:06 -0300

    delete screen main, add new script from routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,8 +1,6 @@
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-import Main from '../Screens/Main';
 import SignIn from '../Screens/Auth/Login';
 import { propsNavigationStack } from '../utils/routeProps';
 
@@ -13,16 +11,16 @@ export default function Routes(){
   return(
     <Stack.Navigator>
       <Stack.Screen
-        name="Main"
-        component={Main}
-        options={{headerShown: false}}
-      />
-
-      <Stack.Screen
         name="SignIn"
         component={SignIn}
         options={{headerShown: false}}
       />
+
+      {/* <Stack.Screen
+        name="registration"
+        component={registration}
+        options={{headerShown: false}}
+      /> */}
     </Stack.Navigator>
   );
 }
